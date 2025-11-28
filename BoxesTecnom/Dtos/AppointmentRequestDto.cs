5a81c281ac044fc9375dbfe6c647931973113fd6
Martin Echeverria <martin.echeverria@accenture.com>
2025-11-27 23:01:39 -0300

    Add all files

--- BoxesTecnom/Dtos/AppointmentRequestDto.cs
+++ BoxesTecnom/Dtos/AppointmentRequestDto.cs
@@ -6,13 +6,13 @@ namespace BoxesTecnom.Dtos
     public class AppointmentRequestDto
     {
         [JsonPropertyName("place_id")]
-        public required int Place_Id { get; set; }
+        public required int Place_id { get; set; }
 
         [JsonPropertyName("appointment_at")]
-        public required DateTime Appointment_At { get; set; }
+        public required DateTime Appointment_at { get; set; }
 
         [JsonPropertyName("service_type")]
-        public required string Service_Type { get; set; }
+        public required string Service_type { get; set; }
 
         public required ContactDto Contact { get; set; }
 
--- BoxesTecnom/Models/Appointment.cs
+++ BoxesTecnom/Models/Appointment.cs
@@ -2,9 +2,9 @@
 {
     public class Appointment
     {
-        public required int Place_Id { get; set; }
-        public required DateTime Appointment_At { get; set; }
-        public required string Service_Type { get; set; }
+        public required int Place_id { get; set; }
+        public required DateTime Appointment_at { get; set; }
+        public required string Service_type { get; set; }
         public required Contact Contact { get; set; }
         public Vehicle? Vehicle { get; set; }
     }
